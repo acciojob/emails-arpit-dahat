9a7372ad7da6e6a2b93d3effe746cb206949c198
Arpit <arpitdahat.ad@gmail.com>
2022-12-25 01:10:45 +0530

    Update

---
+++ src/main/java/com/driver/Mail.java
@@ -0,0 +1,25 @@
+package com.driver;
+
+import java.util.Date;
+
+public class Mail {
+
+    private Object message;
+
+    public Mail(Date date, String sender, String message) {
+
+    }
+
+    public Object getMessage() {
+        Object message = null;
+        return message;
+    }
+
+    public void setMessage(Object message) {
+        this.message = message;
+    }
+
+    public Comparable<Date> getDate() {
+        return getDate();
+    }
+}
